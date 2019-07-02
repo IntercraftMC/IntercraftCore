687a6cc3d6ef29b1aaae8a7821ac40ec2e191127
Shazuli <sigge-e@hotmail.com>
2019-07-02 16:28:09 +0200

    Now drops correct amount, added block item model.

--- src/main/java/net/intercraft/intercraftcore/ore/BlockHardOre.java
+++ src/main/java/net/intercraft/intercraftcore/ore/BlockHardOre.java
@@ -1,6 +1,5 @@
 package net.intercraft.intercraftcore.ore;
 
-import net.intercraft.intercraftcore.init.IntercraftItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
@@ -19,20 +18,20 @@ import java.util.Random;
 public class BlockHardOre extends Block {
 
     private Item drop;
-    private int color;
+    private int tint;
 
 
-    public BlockHardOre(final String name, final float hardness, final float resistance, Item drop, int color) {
+    public BlockHardOre(final String name, final float hardness, final float resistance, Item drop, int tint) {
         super(Block.Properties.create(Material.ROCK, MaterialColor.STONE).hardnessAndResistance(hardness,resistance));
 
         setRegistryName(name+"_ore");
         this.drop = drop;
-        this.color = color;
+        this.tint = tint;
     }
 
 
-    public int getColor() {
-        return this.color;
+    public int getTint() {
+        return this.tint;
     }
 
     protected ItemStack getSilkTouchDrop(IBlockState state) {
@@ -43,7 +42,7 @@ public class BlockHardOre extends Block {
         return this.drop;
     }
     public int getItemsToDropCount(IBlockState state, int fortune, World worldIn, BlockPos pos, Random random) {
-        int toRet = 4;
+        int toRet = state.get(BlockProperties.DENSITY)+1;
 
         return Math.max(toRet,1);
 
